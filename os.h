df6c8072393964e1054ba96a3352cdd13c480b56
SonnyJim <>
2025-05-04 12:01:37 +1000

    Missed a sopt

--- os.h
+++ os.h
@@ -1,4 +1,4 @@
-int scsi_open(char *path);
+int scsi_open(char *path, int readonly);
 int scsi_send_command(int dev, unsigned char *cmd, int cmd_len, unsigned char *buf, int buf_len);
 int scsi_send_commandw(int dev, unsigned char *cmd, int cmd_len, unsigned char *buf, int buf_len);
 int scsi_close(int dev);
